199377a98608e78c8369e3acc7ceaa40bd4811e1
Dongie Agnir <dongie@amazon.com>
2019-02-12 14:28:40 -0800

    wip

--- tests/integration/workflows/java_gradle/test_path_resolution.py
+++ tests/integration/workflows/java_gradle/test_path_resolution.py
@@ -10,5 +10,5 @@ class TestPathResolution(TestCase):
         self.search_path = os.path.join(os.path.dirname(__file__), "testdata", "single-build", "with-deps-gradlew")
 
     def test_gradlew_resolution(self):
-        r = PathResolver(binary='gradlew', runtime=None, executable_search_paths=[self.search_path])
+        r = PathResolver(binary='gradlew.bat', runtime=None, executable_search_paths=[self.search_path])
         self.assertEqual(len(r.exec_paths), 1)

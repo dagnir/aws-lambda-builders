80b1018ab57c8fcfcdb0fa52d16ade82e4fbab54
Dongie Agnir <dongie@amazon.com>
2019-02-12 14:27:46 -0800

    wip

--- tests/integration/workflows/java_gradle/test_path_resolution.py
+++ tests/integration/workflows/java_gradle/test_path_resolution.py
@@ -3,11 +3,11 @@ from unittest import TestCase
 
 from aws_lambda_builders.path_resolver import PathResolver
 
+
 class TestPathResolution(TestCase):
 
     def setUp(self):
         self.search_path = os.path.join(os.path.dirname(__file__), "testdata", "single-build", "with-deps-gradlew")
-
 
     def test_gradlew_resolution(self):
         r = PathResolver(binary='gradlew', runtime=None, executable_search_paths=[self.search_path])
